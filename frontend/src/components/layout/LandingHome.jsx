07513b23cf99e51a02da46df0bcad9ada5ad4e6c
Debabrata Giri <debabratagiri5525@gmail.com>
2024-04-12 14:18:51 +0530

    miner changes in all

--- frontend/src/components/layout/LandingHome.jsx
+++ frontend/src/components/layout/LandingHome.jsx
@@ -11,7 +11,7 @@ const LandingHome = () => {
             <img src={Shape2} alt="shape2" className='absolute -right-1/4 top-2/3 w-1/2 h-1/2 opacity-70' />
             <img src={Shape3} alt="shape3" className='absolute left-2/4 top-1 w-1/2 h-1/2 opacity-70' />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-color3 font-Ubuntu">Welcome Online Shopping</h1>
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-color3 font-Ubuntu">Welcome.</h1>
                 <div className="flex justify-center mt-3">
                     <div className="animate-bounce ">
                         <BiSolidMouse size={'34'} color='#009dc0' />
