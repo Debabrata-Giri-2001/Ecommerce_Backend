bf926d281e7819e4a2d130ccf4e677e7fe037e35
Debabrata Giri <debabratagiri5525@gmail.com>
2024-04-14 21:58:46 +0530

    added products details page

--- frontend/src/routes/PrivateRt.jsx
+++ frontend/src/routes/PrivateRt.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/common/Home';
+import ProductDetails from '../pages/common/ProductDetails';
 
 const PrivateRt = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
       </Routes>
     </Router>
   )
