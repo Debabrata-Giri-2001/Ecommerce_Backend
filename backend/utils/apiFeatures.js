671e58b12074ab8ef54e721e0eb4fa42cda07e38
Debabrata Giri <debabratagiri5525@gmail.com>
2024-05-08 17:24:37 +0530

    added extra query options

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -7,10 +7,11 @@ class Applications {
     searchData() {
         const keyword = this.queryStr.keyword ?
             {
-                name: {
-                    $regex: this.queryStr.keyword,
-                    $options: 'i'
-                }
+                $or: [
+                    { name: { $regex: this.queryStr.keyword, $options: 'i' } },
+                    { description: { $regex: this.queryStr.keyword, $options: 'i' } },
+                    { category: { $regex: this.queryStr.keyword, $options: 'i' } }
+                ]
             }
             : {};
         this.query = this.query.find({ ...keyword })
@@ -23,16 +24,16 @@ class Applications {
         const removeField = ["keyword", "page", "limit"]
         removeField.forEach(key => delete queryCopy[key])
         //filter for price range
-        
+
         let queryStr = JSON.stringify(queryCopy);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key)=>`$${key}`);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
 
         this.query = this.query.find(JSON.parse(queryStr));
 
         return this;
     }
 
-    pagination(resultPerPage){
+    pagination(resultPerPage) {
         const currentPage = Number(this.queryStr.page) || 1;
         const skip = resultPerPage * (currentPage - 1);
 
