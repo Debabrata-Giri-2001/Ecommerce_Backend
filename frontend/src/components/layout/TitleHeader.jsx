1c2301d4f732d7e1c14459025a8f41b6c1b3754c
Debabrata Giri <debabratagiri5525@gmail.com>
2024-05-21 10:41:39 +0530

    new products and title components

---
+++ frontend/src/components/layout/TitleHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TitleHeader = ({title}) => {
+    return (
+        <div className="text-3xl font-bold text-gray-500 font-Kanit py-2 mx-3">
+            {title}
+        </div>
+    )
+}
+
+export default TitleHeader
