ad1b0b569de4f303cb940839933e01342926787f
Debabrata Giri <debabratagiri@email.com>
2024-05-27 16:24:26 +0530

    added tile in menu

--- frontend/src/pages/admin/SideBar.jsx
+++ frontend/src/pages/admin/SideBar.jsx
@@ -21,14 +21,14 @@ const SideBar = () => {
       <Menu style={{ marginTop: '5rem' }}>
         <p className='px-3 text-gray-400 text-md'>General</p>
         <MenuItem icon={<FaHome />} component={<Link to="/admin/dashboard" />}> Documentation</MenuItem>
-        <MenuItem icon={<FaBox />} component={<Link to="/admin/new-product" />}> Documentation</MenuItem>
-        <MenuItem icon={<MdList />} component={<Link to="/admin/order-list" />}> Documentation</MenuItem>
-        <MenuItem icon={<GiCycle />} component={<Link to="/admin/process-order" />}> Documentation</MenuItem>
-        <MenuItem icon={<MdList />} component={<Link to="/admin/product-list" />}> Documentation</MenuItem>
-        <MenuItem icon={<MdReviews />} component={<Link to="/admin/product-reviews" />}> Documentation</MenuItem>
-        <MenuItem icon={<MdTipsAndUpdates />} component={<Link to="/admin/update-product" />}> Documentation</MenuItem>
-        <MenuItem icon={<FaUserEdit />} component={<Link to="/admin/update-user" />}> Documentation</MenuItem>
-        <MenuItem icon={<FiUsers />} component={<Link to="/admin/users-list" />}> Documentation</MenuItem>
+        <MenuItem icon={<FaBox />} component={<Link to="/admin/new-product" />}>New Product</MenuItem>
+        <MenuItem icon={<MdList />} component={<Link to="/admin/order-list" />}>Order List</MenuItem>
+        <MenuItem icon={<GiCycle />} component={<Link to="/admin/process-order" />}>Process Order</MenuItem>
+        <MenuItem icon={<MdList />} component={<Link to="/admin/product-list" />}>Product List</MenuItem>
+        <MenuItem icon={<MdReviews />} component={<Link to="/admin/product-reviews" />}>Product Reviews</MenuItem>
+        <MenuItem icon={<MdTipsAndUpdates />} component={<Link to="/admin/update-product" />}>Update Product</MenuItem>
+        <MenuItem icon={<FaUserEdit />} component={<Link to="/admin/update-user" />}>Update User</MenuItem>
+        <MenuItem icon={<FiUsers />} component={<Link to="/admin/users-list" />}>User List</MenuItem>
 
       </Menu>
     </Sidebar>
