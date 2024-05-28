5f2016994012b9d5f996ebdf8d75edc3e23503fc
Debabrata Giri <debabratagiri@email.com>
2024-05-28 12:14:44 +0530

    miner changes in auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,41 @@
 const User = require("../model/userModel");
 const ErrorHandler = require("../utils/errorHnadeler");
 const catchAsyncError = require("./catchAsyncError");
-const JWT = require('jsonwebtoken')
-
+const JWT = require('jsonwebtoken');
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+    if (!req.headers) {
+        return next(new ErrorHandler('Please Login to access this resource', 404));
+    }
     const token  = req.headers.authorization.replace(/^Bearer\s+/i, '');
+    
     if (!token) {
-        return next(new ErrorHandler('Please Login to access this resorce', 404))
+        return next(new ErrorHandler('Please Login to access this resource', 404));
     }
-    const decodeData = JWT.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ _id: decodeData.id });
-    if (!user) {
-        return next(new ErrorHandler('User not found', 404));
+    if (!token) {
+        return next(new ErrorHandler('Please Login to access this resource', 404));
     }
-    req.user = user;
-    next();
-})
+
+    try {
+        const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decodeData.id });
+
+        if (!user) {
+            return next(new ErrorHandler('User not found', 404));
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return next(new ErrorHandler('Invalid or expired token', 401));
+    }
+});
 
 exports.authorizeRoles = (...roles) => {
-    return async (req, res, next) => {
+    return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            return next(new ErrorHandler(`Role ${req.user.role} is not allowed to access the resource`, 403));
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed to access this resource`, 403));
         }
         next();
     };
 };
-
